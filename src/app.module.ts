efde10d04e606839a8aa4e8ea1caca404398c6f6
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-03-07 11:31:48 +0300

    create Role module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import ormconfig from './ormconfig'
+import { RoleModule } from './roles/role.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormconfig), RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
