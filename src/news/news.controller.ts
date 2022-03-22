b42844f6e4f8a1c2f420c7c06a0b139452f46ee6
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-03-22 13:24:31 +0300

    add file interceptor utils

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,10 +1,14 @@
 import { User } from "@app/user/decorators/user.decorator";
 import { AuthGuard } from "@app/user/guards/auth.guard";
 import { UserEntity } from "@app/user/user.entity";
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
 import { CreateNewsDto } from "./dto/createNews.dto";
+import { NewsEntity } from "./news.entity";
 import { NewsService } from "./news.service";
 import { NewsResponseInterface } from "./types/newsResponse.interface";
+import { editFileName, imageFileFilter } from "./utils/fileUpload.utils";
 
 @Controller('news')
 export class NewsController {
@@ -19,8 +23,28 @@ export class NewsController {
   }
 
   @Post()
-  @UseGuards(AuthGuard)
-  async createNews(@User() user: UserEntity, @Body('news') createNewsDto: CreateNewsDto): Promise<NewsResponseInterface> {
-    return this.newsService.buildResponse(await this.newsService.createNews(user, createNewsDto))
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'photo', maxCount: 5 },
+        { name: 'preview', maxCount: 1 }
+      ],
+      {
+        storage: diskStorage({
+          destination: './uploads/news',
+          filename: editFileName
+        }),
+        fileFilter: imageFileFilter
+      }
+    )
+  )
+  async tester(
+    @UploadedFiles() files: { photo: Express.Multer.File[], preview: Express.Multer.File },
+    @Body() createNewsDto: CreateNewsDto,
+    @User() user: UserEntity
+  ) {
+    const news = await this.newsService.createNews(files, createNewsDto, user)
+
+    return { news }
   }
 }
