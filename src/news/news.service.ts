ff1408200da6421be62627c57834da3307538966
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-03-22 13:24:54 +0300

    make news

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -6,9 +6,14 @@ import { CreateNewsDto } from "./dto/createNews.dto";
 import { NewsEntity } from "./news.entity";
 import { NewsResponseInterface } from "./types/newsResponse.interface";
 import slugify from "slugify";
+import { convertingFormatFile, convertingFormatFiles } from "./utils/fileUpload.utils";
+import { AuthorType } from "./types/author.type";
+
+
 
 @Injectable()
 export class NewsService {
+
   constructor(
     @InjectRepository(NewsEntity)
     private readonly newsRepository: Repository<NewsEntity>
@@ -18,21 +23,48 @@ export class NewsService {
     return await this.newsRepository.find()
   }
 
-  async createNews(user: UserEntity, createNewsDto: CreateNewsDto): Promise<NewsEntity> {
+  async createNews(
+    files: { photo: Express.Multer.File[], preview: Express.Multer.File }, 
+    createNewsDto: CreateNewsDto, 
+    user: UserEntity
+  ): Promise<NewsEntity> {
     const news = new NewsEntity()
 
     Object.assign(news, createNewsDto)
 
     news.slug = this.getSlug(createNewsDto.title)
 
+    delete user.accessToken
+    delete user.refreshToken
+    delete user.phone
+    delete user.isPhoneActive
+    delete user.isEmailActive
+
     news.author = user
+
+    const photos = await convertingFormatFiles(files.photo)
+    const preview = await convertingFormatFile(files.preview[0])
+
+    news.photos = []
+    photos.forEach(photo => {
+      news.photos.push({
+        filename: photo.filename,
+        path: `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_IP}:${process.env.SERVER_PORT}/${photo.path}`
+      })
+    })
+
+    news.preview = {
+      filename: preview.filename,
+      path: `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_IP}:${process.env.SERVER_PORT}/${preview.path}`
+    }
 
     return await this.newsRepository.save(news)
   }
 
-  buildResponse(news: NewsEntity | NewsEntity[]): NewsResponseInterface {
+  buildResponse(news: NewsEntity[]): NewsResponseInterface {
     return {
-      news
+      news,
+      newsCount: news.length
     }
   }
 
