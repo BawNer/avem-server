14c09b9a798f76bb7dd236c5dde85bde7299e602
BawNer <bauner@yandex.ru>
2022-04-05 18:38:32 +0300

    Добавлены фильтры по аудиториям и преподавателям

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -5,16 +5,16 @@ import { DbfParserUtils } from "./utils/dbfParser.utils";
 
 @Injectable()
 export class ScheduleService {
-  async getSchedule(query?: any): Promise<any> {
+  async getSchedule(query: any): Promise<any> {
     const {groupsId, schedule} = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', '/uploads/schedule/schema.json'), {encoding: 'utf-8'}))
 
     let schema = schedule
 
-    if (query?.group) {
+    if (query.group) {
       schema = schema.filter(item => item.group == query.group)
     }
 
-    if (query?.date == 'today') {
+    if (query.date == 'today') {
       for (const item of schema) {
         item.schema = item.schema.filter(item => {
           const currentDate = new Date().toLocaleDateString()
@@ -22,6 +22,26 @@ export class ScheduleService {
           return currentDate == itemDate ? item : false
         })
       }
+    }
+
+    if (query.audience) {
+      for (let i = 0; i < schema.length; i++) {
+        for (const item of schema[i].schema) {
+          item.classes = item.classes.filter(classes => classes.audience == query.audience)
+        }
+        schema[i].schema = schema[i].schema.filter(sh => sh.classes.length)
+      }
+      schema = schema.filter(cl => cl.schema.length)
+    }
+
+    if (query.lecturer) {
+      for (let i = 0; i < schema.length; i++) {
+        for (const item of schema[i].schema) {
+          item.classes = item.classes.filter(classes => classes.lecturer.includes(query.lecturer))
+        }
+        schema[i].schema = schema[i].schema.filter(sh => sh.classes.length)
+      }
+      schema = schema.filter(cl => cl.schema.length)
     }
 
     return {
@@ -34,7 +54,7 @@ export class ScheduleService {
     const dbfParser = new DbfParserUtils(schema)
     let parsedSchema = {}
     await dbfParser.parse().then(() => {
-      parsedSchema = this.getSchedule()
+      parsedSchema = this.getSchedule({})
     })
 
     return parsedSchema
