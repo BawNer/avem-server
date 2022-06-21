7dff60fb141fea1faf08f48b32e531c9b7424bb1
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-06-21 16:43:59 +0300

    add events, audience

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,35 +5,39 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GroupModule } from './group/group.module';
 import { NewsModule } from './news/news.module';
-import ormconfig from './ormconfig'
+import ormconfig from './ormconfig';
 import { RoleModule } from './role/role.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { TokenModule } from './token/token.module';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
 import { UserModule } from './user/user.module';
+import { EventsModule } from '@app/events/events.module';
+import { AudienceModule } from '@app/audience/audience.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
     ThrottlerModule.forRoot({
       ttl: 60,
-      limit: 10
+      limit: 10,
     }),
-    RoleModule, 
-    GroupModule, 
+    RoleModule,
+    GroupModule,
     NewsModule,
-    UserModule, 
+    UserModule,
     TokenModule,
-    ScheduleModule
+    ScheduleModule,
+    EventsModule,
+    AudienceModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AuthMiddleware).forRoutes({
       path: '*',
-      method: RequestMethod.ALL
-    })
+      method: RequestMethod.ALL,
+    });
   }
 }
